2c9fc7af5bff78c003413f59cc27f19d0002e9ac
amandalyckenius <alyckenius@gmail.com>
2025-05-08 11:49:41 +0200

    initial commit

---
+++ src/components/CustomButton.test.tsx
@@ -0,0 +1,66 @@
+//Important steps:
+// Does: describe exists?, it exists?, test exists? getByText? render()?, expect().toBe()
+
+import CustomButton, { ButtonState } from "./customButton"
+import {fireEvent, render } from "@testing-library/react" //isnt found, must manually be imported
+import "@testing-library/jest-dom"
+
+ 
+
+
+//DisplayName() JUnit == collection of test, en metod med multipla asserts
+
+describe("Easy Examples", () => {
+    
+    test("2 + 2 = 4", () => {
+        expect(2+2).toBe(4)
+    })
+    test("is 2+4 greater than 0", () => {
+        expect(2+4).toBeGreaterThan(0)
+    })
+
+    test("2-2 equals 0", () => {
+        expect(2-2).toBe(0)
+    })
+})    
+
+describe("Custom Button Component States", () => {
+
+    test("Check if button Component starts with idle", () => {
+        const buttonText: string = "signup"
+        const {getByText} = render(<CustomButton buttonText={buttonText}/>)
+        const customButtonComponent = getByText(buttonText)
+
+        expect(customButtonComponent).toHaveClass(ButtonState.idle)
+    })
+
+    test("Check if Button Component on hover state changes", () => {
+        const buttonText: string = "signup"
+        const {getByText} = render(<CustomButton buttonText={buttonText}/>)
+        const customButtonComponent = getByText(buttonText)
+
+        expect(customButtonComponent).toHaveClass(ButtonState.idle)
+        fireEvent.mouseEnter(customButtonComponent)
+        expect(customButtonComponent).toHaveClass(ButtonState.hover)
+    })
+
+    test("Custom Button Component on click state change", () => {
+        const buttonText: string = "signup"
+        const {getByText} = render(<CustomButton buttonText={buttonText}/>)
+        const customButtonComponent = getByText(buttonText)
+
+        expect(customButtonComponent).toHaveClass(ButtonState.idle)
+        fireEvent.click(customButtonComponent)
+        expect(customButtonComponent).toHaveClass(ButtonState.clicked)
+    })
+
+})
+
+
+
+
+
+
+it("", () => {})
+test("", () => {})
+
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,35 @@
+//TODO - have we interavted with the button?
+//Check for states, for hover, clicked and idle
+//Classname TEST
+
+import { useState } from "react"
+
+//ENUM
+//IDLE HOVER CLICKED
+export enum ButtonState {
+    idle = "idle",
+    hover ="hover",
+    clicked = "clicked",
+}
+
+// You lose on: refactor capabilities, immutability - gain speed
+type ButtonStateTwo = "idle" | "hover" | "clicked"
+
+interface CustomButtonProps {
+    buttonText: string
+}
+
+export default function CustomButton(props: CustomButtonProps){
+    const [buttonState, setButtonState] = useState<ButtonState>(ButtonState.idle)
+    const [buttonStateTwo, setButtonStateTwo] = useState<ButtonStateTwo>("idle")
+    
+    return (
+    <input 
+        type="button" 
+        value={props.buttonText} 
+        className={buttonState}
+        onMouseLeave={() => {setButtonState(ButtonState.idle)}}
+        onClick={() => {setButtonState(ButtonState.clicked)}}
+        onMouseEnter={() => {setButtonState(ButtonState.hover)}}/>
+    )
+}
